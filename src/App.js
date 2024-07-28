43e2c591c33c1be2f433e885214e33a867b05de4
maebo <maeboittiaux@gmail.com>
2024-07-28 17:08:59 +0100

    Add div components to break up sections

--- src/App.js
+++ src/App.js
@@ -4,61 +4,67 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Scon-epedia</h1>
+      <div className='title-banner'>
+        <h1>Scon-epedia</h1>
+      </div>
 
       <h2>Classic Cheese Scones</h2>
 
-      <h3>Ingredients</h3>
-      <hr />
-      <p>225g self-raising flour, plus extra for dusting</p>
-      <hr />
-      <p>Pinch of salt</p>
-      <hr />
-      <p>Pinch of cayenne pepper</p>
-      <hr />
-      <p>1 tsp baking powder</p>
-      <hr />
-      <p>55g chilled butter, cut into cubes</p>
-      <hr />
-      <p>120g mature cheddar, grated</p>
-      <hr />
-      <p>90-100ml milk, plus 1 tbsp for glazing</p>
+      <div className='ingredients'>
+        <h3>Ingredients</h3>
+        <hr />
+        <p>225g self-raising flour, plus extra for dusting</p>
+        <hr />
+        <p>Pinch of salt</p>
+        <hr />
+        <p>Pinch of cayenne pepper</p>
+        <hr />
+        <p>1 tsp baking powder</p>
+        <hr />
+        <p>55g chilled butter, cut into cubes</p>
+        <hr />
+        <p>120g mature cheddar, grated</p>
+        <hr />
+        <p>90-100ml milk, plus 1 tbsp for glazing</p>
+      </div>
 
-      <h3>Method</h3>
-      <h4>Step 1</h4>
-      <p>Heat the oven to 200C/180C fan/gas 6 with a large
-        baking tray inside. Sift the flour, salt, cayenne pepper
-        and baking powder into a bowl, then sift again to make
-        sure the ingredients are thoroughly combined.
-      </p>
+      <div className='method'>
+        <h3>Method</h3>
+        <h4>Step 1</h4>
+        <p>Heat the oven to 200C/180C fan/gas 6 with a large
+          baking tray inside. Sift the flour, salt, cayenne pepper
+          and baking powder into a bowl, then sift again to make
+          sure the ingredients are thoroughly combined.
+        </p>
 
-      <h4>Step 2</h4>
-      <p>Add the butter to the bowl and combine with your
-        fingertips to make breadcrumbs. Sprinkle 100g of the
-        cheese into the breadcrumb mixture and rub together
-        until evenly distributed. Try not to mix too much as the
-        heat from your hands may start to melt the butter.
-      </p>
+        <h4>Step 2</h4>
+        <p>Add the butter to the bowl and combine with your
+          fingertips to make breadcrumbs. Sprinkle 100g of the
+          cheese into the breadcrumb mixture and rub together
+          until evenly distributed. Try not to mix too much as the
+          heat from your hands may start to melt the butter.
+        </p>
 
-      <h4>Step 3</h4>
-      <p>Make a well in the centre of the mixture and pour in
-        enough milk to give a fairly soft but firm dough. Do not
-        pour in all the milk at once as you may not need it all to
-        get the right consistency.
-      </p>
+        <h4>Step 3</h4>
+        <p>Make a well in the centre of the mixture and pour in
+          enough milk to give a fairly soft but firm dough. Do not
+          pour in all the milk at once as you may not need it all to
+          get the right consistency.
+        </p>
 
-      <h4>Step 4</h4>
-      <p>Lightly flour a surface and roll out the dough to
-        approximately 2cm thick. Cut out the scones with a
-        medium (about 8cm) cutter, then put on a sheet of
-        baking parchment, glaze with a little milk and sprinkle
-        with the remaining cheese. Slide onto the hot oven tray.
-      </p>
+        <h4>Step 4</h4>
+        <p>Lightly flour a surface and roll out the dough to
+          approximately 2cm thick. Cut out the scones with a
+          medium (about 8cm) cutter, then put on a sheet of
+          baking parchment, glaze with a little milk and sprinkle
+          with the remaining cheese. Slide onto the hot oven tray.
+        </p>
 
-      <h4>Step 5</h4>
-      <p>Bake in the oven for 15-20 mins or until golden brown
-        and cooked through.
-      </p>
+        <h4>Step 5</h4>
+        <p>Bake in the oven for 15-20 mins or until golden brown
+          and cooked through.
+        </p>
+      </div>
     </div>
   );
 }
